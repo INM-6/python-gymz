b16f666af5e7dd1e2201e8e57c0590933b5b73e9
Jakob Jordan <j.jordan@fz-juelich.de>
2017-06-07 20:06:06 +0200

    Add setup script

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='gymz',
+    version='0.0.1',
+    author='Jakob Jordan, Philipp Weidel',
+    author_email='j.jordan@fz-juelich.de',
+    description=('A light-weight ZMQ wrapper for the OpenAI Gym.'),
+    license='MIT',
+    keywords='openai-gym reinforcement-learning zmq',
+    url='https://github.com/INM-6/python-gymz',
+    packages=['gymz', 'examples'],
+    scripts=['gymz-controller'],
+    data_files=['DefaultConfig.json'],
+    long_description=open('README.md').read(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+    ],
+)
