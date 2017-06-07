79991de62aaf4048a2de32288378a1da2a8cb21c
Jakob Jordan <j.jordan@fz-juelich.de>
2017-06-07 22:12:03 +0200

    Add requirements to setup.py, remove requirements file

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ setup(
     author='Jakob Jordan, Philipp Weidel',
     author_email='j.jordan@fz-juelich.de',
     description=('A light-weight ZeroMQ wrapper for the OpenAI Gym.'),
+    long_description=open('README.md').read(),
     license='MIT',
     keywords='openai-gym reinforcement-learning zeromq zmq',
     url='https://github.com/INM-6/python-gymz',
     packages=['gymz'],
     scripts=['gymz-controller'],
     data_files=[('', ['DefaultConfig.json'])],
-    long_description=open('README.md').read(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
@@ -22,4 +22,10 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Topic :: Scientific/Engineering',
     ],
+    install_requires=[
+        'docopt',
+        'gym>=0.8.1',
+        'numpy',
+        'pyzmq',
+    ]
 )
