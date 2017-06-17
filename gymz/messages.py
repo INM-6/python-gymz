a1b2597be105809fa6e7a44813dd19f08a28d7f3
Jakob Jordan <j.jordan@fz-juelich.de>
2017-06-17 23:55:23 +0200

    Add assert to to_message function to avoid sending wrongly formatted messages

--- gymz/messages.py
+++ gymz/messages.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
+import numpy as np
+
 
 def to_message(low, high, value):
+    assert(np.shape(low) == np.shape(high) == np.shape(value))
     try:
         return [{'min': low[i], 'max': high[i], 'value': value[i]} for i in xrange(len(low))]
     except TypeError:
