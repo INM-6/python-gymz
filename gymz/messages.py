8062b2bc4de7120be69b3a7290a68eb0230da9df
Jakob Jordan <jakobjordan@posteo.de>
2019-05-26 13:51:48 +0200

    Convert numpy types to native types for messages

--- gymz/messages.py
+++ gymz/messages.py
@@ -5,7 +5,28 @@ import numpy as np
 
 def to_message(low, high, value):
     assert(np.shape(low) == np.shape(high) == np.shape(value))
+
     try:
+
+        # convert numpy arrays to lists as they can not be json serialized
+        try:
+            low = low.tolist()
+            high = high.tolist()
+            value = value.tolist()
+        except:
+            pass
+
         return [{'min': low[i], 'max': high[i], 'value': value[i]} for i in range(len(low))]
+
     except TypeError:
+
+        # convert numpy types to native types as they can not be json
+        # serialized
+        try:
+            low = low.item()
+            high = high.item()
+            value = value.item()
+        except:
+            pass
+
         return [{'min': low, 'max': high, 'value': value}]
